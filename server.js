b1a643490c0982ecd76609205106ebd4b7065ac3
ayotayo <ayotayobadejo@gmail.com>
2025-06-21 18:44:03 -0400

    create project form

--- server.js
+++ server.js
@@ -28,14 +28,16 @@ console.log("🔄 Setting up middleware...");
 // Security middleware
 app.use(helmet());
 
-const allowedOrigins = [process.env.FRONTEND_URL, "http://localhost:1573"];
+// const allowedOrigins = [process.env.FRONTEND_URL, "http://localhost:1573"];
 
-// app.use(
-//   cors({
-//     origin: process.env.FRONTEND_URL || "http://localhost:1573",
-//     credentials: true,
-//   })
-// );
+app.use(
+  cors({
+    origin:
+      // process.env.FRONTEND_URL ||
+      "http://localhost:1573",
+    credentials: true,
+  })
+);
 
 app.use(
   cors({
@@ -64,11 +66,11 @@ app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
 
 console.log("🔄 Setting up routes...");
-app.get('/',(req, res) => {
+app.get("/", (req, res) => {
   res.status(201).json({
-      message: 'welcome to bitstock api '
-  })
-})
+    message: "welcome to bitstock api ",
+  });
+});
 // Health check endpoint (simple route first)
 app.get("/health", (req, res) => {
   res.status(200).json({
